a11b7fc094602165de833591e852e58a3de8ef35
nilsboi <88249124+nilsboi@users.noreply.github.com>
2024-08-12 11:17:00 +0200

    Some exercises

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,13 +1,17 @@
 package database
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"log"
 	"os"
 	"strconv"
 	"sync"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -17,71 +21,93 @@ type DB struct {
 }
 
 type DBStructure struct {
-	Chirps map[int]Chirp `json:"chirps"`
-	Users map[int]User `json:"users"`
+	Chirps map[int]Chirp    `json:"chirps"`
+	Users  map[int]User     `json:"users"`
+	Tokens map[string]Token `json:"tokens"`
 }
 
 type Chirp struct {
-	ID   int    `json:"id"`
-	Body string `json:"body"`
+	ID     int    `json:"id"`
+	Body   string `json:"body"`
+	Author int    `json:"author_id"`
 }
 
+type Token struct {
+	TokenString string    `json:"tokenString"`
+	Expires     time.Time `json:"expires"`
+	UserID      int       `json:"user"`
+}
 
 type User struct {
-	ID   int    `json:"id"`
-	Email string `json:"email"`
-	Password *string `json:"password,omitempty"`
+	ID           int     `json:"id"`
+	Email        string  `json:"email"`
+	Password     *string `json:"password,omitempty"`
+	Token        string `json:"token,omitempty"`
+	RefreshToken string `json:"refresh_token,omitempty"`
 }
 
 // CreateChirp creates a new chirp and saves it to disk
-func (db *DB) CreateChirp(body string) (Chirp, error) {
-	
+func (db *DB) CreateChirp(body string, token string) (Chirp, error) {
 
-	chirps, err1 := db.GetChirps()
+	users, err := db.GetUsers()
 
-	if err1 != nil {
-		log.Printf("Error fetching chirps in database: %v", err1)
-		return Chirp{}, err1
-	}
-	max := 0
-
-	for _, chirp := range chirps {
-    if chirp.ID > max {
-        max = chirp.ID
-    }
+	if err != nil {
+		log.Printf("Error fetching users in database: %v", err)
+		return Chirp{}, err
 	}
 
-	id := max +1
+	for _, user := range users {
+		
+		if user.Token == token {
+			chirps, err1 := db.GetChirps(0)
 
-	chirp := Chirp{
-		ID: id,
-		Body: body,
+			if err1 != nil {
+				log.Printf("Error fetching chirps in database: %v", err1)
+				return Chirp{}, err1
+			}
+			max := 0
+
+			for _, chirp := range chirps {
+				if chirp.ID > max {
+					max = chirp.ID
+				}
+			}
+
+			id := max + 1
+
+			chirp := Chirp{
+				ID:   id,
+				Body: body,
+				Author: user.ID,
+			}
+
+			dbStructure, err2 := db.loadDB()
+
+			if err2 != nil {
+				log.Printf("Error reading database file: %v", err2)
+				return Chirp{}, err2
+			}
+
+			dbStructure.Chirps[id] = chirp
+
+			err3 := db.writeDB(dbStructure)
+
+			if err3 != nil {
+				log.Printf("Error writing database file: %v", err3)
+				return Chirp{}, err3
+			}
+
+			return chirp, nil
+
+		}
 	}
-
-	dbStructure, err2 := db.loadDB()
-
-	if err2 != nil {
-		log.Printf("Error reading database file: %v", err2)
-		return Chirp{}, err2
-	}
-
-	dbStructure.Chirps[id] = chirp
-
-	err3 := db.writeDB(dbStructure) 
-
-	if err3 != nil {
-		log.Printf("Error writing database file: %v", err3)
-		return Chirp{}, err3
-	}
-	
-
-	return chirp, nil 
+	return Chirp{}, errors.New("unauthorized")
 }
 
 // GetChirps returns all chirps in the database
-func (db *DB) GetChirps() ([]Chirp, error) {
+func (db *DB) GetChirps(id int) ([]Chirp, error) {
 	db.mux.RLock()
-defer db.mux.RUnlock()
+	defer db.mux.RUnlock()
 	dbStructure, err := db.loadDB()
 
 	if err != nil {
@@ -89,13 +115,23 @@ defer db.mux.RUnlock()
 		return nil, err
 	}
 
-	chirps := make([]Chirp, 0, len(dbStructure.Chirps))
+	chirps := []Chirp{}
 
-	for _, chirp := range dbStructure.Chirps {
-    chirps = append(chirps, chirp)
+	if id == 0 {
+
+		for _, chirp := range dbStructure.Chirps {
+			chirps = append(chirps, chirp)
+		}
+	} else {
+		for _, chirp := range dbStructure.Chirps {
+			if id == chirp.ID {
+				chirps = append(chirps, chirp)
+			}
+		}
 	}
 
 	return chirps, nil
+
 }
 
 // Get chrips bei id
@@ -110,43 +146,76 @@ func (db *DB) GetChirp(id string) (Chirp, error) {
 		return Chirp{}, err
 	}
 
-	find, err := strconv.Atoi(id) 
-	
+	find, err := strconv.Atoi(id)
+
 	if err != nil {
 		log.Printf("Error casting id in GetChir: %v", err)
 		return Chirp{}, err
 	}
 
 	for _, chirp := range dbStructure.Chirps {
-		
-   if chirp.ID == find {
-		return chirp, nil
-	 }
+
+		if chirp.ID == find {
+			return chirp, nil
+		}
 	}
 
 	return Chirp{}, errors.New("ID not found")
 }
 
+func (db *DB) DeleteChirp(tokenString string) (bool, error) {
+
+	users, err := db.GetUsers()
+
+	if err != nil {
+		log.Printf("Error loading tokens: %v", err)
+		return false, err
+	}
+	
+	for _, user := range(users) {
+		if user.Token == tokenString {
+			
+			dbStructure, err := db.loadDB()
+
+			if err != nil {
+				log.Printf("Error loading db: %v", err)
+				return false, err
+			}
+		
+			if dbStructure.Chirps[len(dbStructure.Chirps)].Author == user.ID { 
+			delete(dbStructure.Chirps , len(dbStructure.Chirps) + 1)
+
+			db.writeDB(dbStructure)
+			return true, nil
+			} else {
+				return false, nil
+			}
+		}
+	}
+
+	return false, nil 
+}
+
 // ensureDB creates a new database file if it doesn't exist
 func (db *DB) ensureDB() error {
 	db.mux.Lock()
-defer db.mux.Unlock()
-	err := os.WriteFile(db.path,[]byte(`{ "chirps": {}, "users": {} }`),0666)
+	defer db.mux.Unlock()
+	err := os.WriteFile(db.path, []byte(`{ "chirps": {}, "users": {}, "tokens": {} }`), 0666)
 
-		if err != nil {
-			return err
-		}
+	if err != nil {
+		return err
+	}
 
-	return nil 
+	return nil
 }
 
 // loadDB reads the database file into memory
 func (db *DB) loadDB() (DBStructure, error) {
 	db.mux.RLock()
-defer db.mux.RUnlock()
+	defer db.mux.RUnlock()
 
 	data, err1 := os.ReadFile(db.path)
-	
+
 	if err1 != nil {
 		log.Printf("Error reading File in loadDB: %v", err1)
 		return DBStructure{}, err1
@@ -154,10 +223,10 @@ defer db.mux.RUnlock()
 
 	var dbStructure DBStructure
 	err2 := json.Unmarshal(data, &dbStructure)
-	
+
 	if err2 != nil {
-			log.Printf("Error unmarshal DB in  loadDB: %v", err2)
-			return DBStructure{}, err2
+		log.Printf("Error unmarshal DB in  loadDB: %v", err2)
+		return DBStructure{}, err2
 	}
 
 	return dbStructure, nil
@@ -166,17 +235,16 @@ defer db.mux.RUnlock()
 // writeDB writes the database file to disk
 func (db *DB) writeDB(dbStructure DBStructure) error {
 	db.mux.Lock()
-defer db.mux.Unlock()
-	
+	defer db.mux.Unlock()
 
 	data, err := json.Marshal(dbStructure)
 	if err != nil {
-			return err
+		return err
 	}
 
 	err = os.WriteFile(db.path, data, 0666)
 	if err != nil {
-			return err
+		return err
 	}
 
 	return nil
@@ -187,13 +255,13 @@ defer db.mux.Unlock()
 func NewDB(path string) (*DB, error) {
 
 	newDatabase := DB{
-		path: path, 
-		mux: &sync.RWMutex{}, 
+		path: path,
+		mux:  &sync.RWMutex{},
 	}
-	
+
 	_, err := os.ReadFile(path)
 
-	if errors.Is(err, os.ErrNotExist ) {
+	if errors.Is(err, os.ErrNotExist) {
 		err := newDatabase.ensureDB()
 		if err != nil {
 			return nil, err
@@ -211,20 +279,20 @@ func (db *DB) CreateUser(email string, password string) (User, error) {
 		log.Printf("Error fetching users in database: %v", err1)
 		return User{}, err1
 	}
-	
+
 	max := 0
 
 	for _, user := range users {
-    if user.ID > max {
-        max = user.ID
-    }
+		if user.ID > max {
+			max = user.ID
+		}
 
 		if user.Email == email {
 			return User{}, errors.New("User already registered")
 		}
 	}
 
-	id := max +1
+	id := max + 1
 
 	password, err4 := HashPassword(password)
 
@@ -234,8 +302,8 @@ func (db *DB) CreateUser(email string, password string) (User, error) {
 	}
 
 	user := User{
-		ID: id,
-		Email: email,
+		ID:       id,
+		Email:    email,
 		Password: &password,
 	}
 
@@ -248,17 +316,17 @@ func (db *DB) CreateUser(email string, password string) (User, error) {
 
 	dbStructure.Users[id] = user
 
-	err3 := db.writeDB(dbStructure) 
+	err3 := db.writeDB(dbStructure)
 
 	if err3 != nil {
 		log.Printf("Error writing database file: %v", err3)
 		return User{}, err3
 	}
-	
+
 	user.Password = nil
 
-	return user, nil 
-	
+	return user, nil
+
 }
 
 func (db *DB) GetUsers() ([]User, error) {
@@ -274,38 +342,239 @@ func (db *DB) GetUsers() ([]User, error) {
 	users := make([]User, 0, len(dbStructure.Chirps))
 
 	for _, user := range dbStructure.Users {
-    users = append(users, user)
+		users = append(users, user)
 	}
 
 	return users, nil
 }
 
-func (db *DB) Login(email string, password string) (User, error) {
-	
+func (db *DB) GetTokens() ([]Token, error) {
+	db.mux.RLock()
+	defer db.mux.RUnlock()
+
+	dbStructure, err := db.loadDB()
+
+	if err != nil {
+		log.Printf("Error fetching users in GetUsers: %v", err)
+		return nil, err
+	}
+
+	tokens := make([]Token, 0, len(dbStructure.Tokens))
+
+	for _, token := range dbStructure.Tokens {
+		tokens = append(tokens, token)
+	}
+
+	return tokens, nil
+
+}
+
+func (db *DB) Login(email string, password string, key string) (User, error) {
+
 	users, err1 := db.GetUsers()
 
 	if err1 != nil {
 		log.Printf("Error fetching users in database: %v", err1)
 		return User{}, err1
 	}
-	
+
 	for _, user := range users {
-    if user.Email == email {
+		if user.Email == email {
 			check := CheckPasswordHash(password, *user.Password)
-			
+
 			if !check {
 				log.Print("Credentials not valid")
 				return User{}, errors.New("Problem with login")
 			}
 
-       user.Password = nil
-			 return user, nil
-			 
-    }
+			claims := &jwt.RegisteredClaims{
+				IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+				ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(time.Second * time.Duration(3600))), //TODO und Expires richtig implementiren
+				Issuer:    "chirpy",
+				Subject:   strconv.Itoa(user.ID),
+			}
+
+			mySigningKey := []byte(key)
+
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+			ss, err := token.SignedString(mySigningKey)
+			if err != nil {
+				log.Print("Error signing token")
+				return User{}, errors.New("Problem with Token")
+			}
+
+			dbStructure, err2 := db.loadDB()
+
+			if err2 != nil {
+				log.Print("Error Loadingdb")
+				return User{}, errors.New("Problem with loading DB")
+			}
+
+			newToken := Token{}
+			bytes := make([]byte, 32) // 256 bits
+			_, err5 := rand.Read(bytes)
+			if err5 != nil {
+				return User{}, err5
+			}
+
+			newToken.TokenString = hex.EncodeToString(bytes)
+			newToken.Expires = time.Now().Add(time.Hour * time.Duration(1440))
+			newToken.UserID = user.ID
+
+			dbStructure.Tokens[hex.EncodeToString(bytes)] = newToken
+
+			user.Token = ss
+			user.RefreshToken = newToken.TokenString
+			
+			dbStructure.Users[user.ID] = user
+
+			err3 := db.writeDB(dbStructure)
+
+			if err3 != nil {
+				log.Printf("Error writing database file: %v", err3)
+				return User{}, err3
+			}
+
+			user.Token = ss
+			user.Password = nil
+			user.RefreshToken = newToken.TokenString
+
+			return user, nil
+
+		}
 	}
+
 	return User{}, errors.New("User not found")
 }
 
+func (db *DB) UpdateUser(email string, password string, id int) (User, error) {
+
+	users, err1 := db.GetUsers()
+
+	if err1 != nil {
+		log.Printf("Error fetching users in database: %v", err1)
+		return User{}, err1
+	}
+
+	for _, user := range users {
+		if user.ID == id {
+
+			dbStructure, err2 := db.loadDB()
+
+			if err2 != nil {
+				log.Printf("Error reading database file: %v", err2)
+				return User{}, err2
+			}
+
+			password, err4 := HashPassword(password)
+
+			if err4 != nil {
+				log.Printf("Error hashing password: %v", err4)
+				return User{}, err4
+			}
+
+			user.Email = email
+			user.Password = &password
+			dbStructure.Users[id] = user
+
+			err3 := db.writeDB(dbStructure)
+
+			if err3 != nil {
+				log.Printf("Error writing database file: %v", err3)
+				return User{}, err3
+			}
+
+			user.Password = nil
+			return user, nil
+
+		}
+	}
+	return User{}, errors.New("problem with updating credentials")
+}
+
+func (db *DB) RefreshToken(refreshToken string, key string) (string, error) {
+	tokens, err := db.GetTokens()
+	if err != nil {
+		log.Printf("Error fetching tokens: %v", err)
+		return "", err
+	}
+
+	for _, token := range tokens {
+		if token.TokenString == refreshToken {
+
+			if time.Now().UTC().After(token.Expires) {
+				return "", errors.New("expired")
+			}
+
+			claims := &jwt.RegisteredClaims{
+				IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+				ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(time.Second * time.Duration(3600))),
+				Issuer:    "chirpy",
+				Subject:   strconv.Itoa(token.UserID),
+			}
+
+			mySigningKey := []byte(key)
+
+			tokenA := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+			ss, err := tokenA.SignedString(mySigningKey)
+			if err != nil {
+				log.Print("Error signing token")
+				return "", errors.New("Problem with access token")
+			}
+
+			dbStructure, err := db.loadDB()
+
+			if err != nil {
+				log.Printf("Error fetching DB: %v", err)
+				return "", err
+			}
+
+			user := dbStructure.Users[token.UserID]
+
+			user.Token = ss
+
+			dbStructure.Users[token.UserID] = user
+
+			err2 := db.writeDB(dbStructure)
+
+			if err2 != nil {
+				log.Print("Error saving access token")
+				return "", errors.New("error saving access token")
+			}
+
+			return ss, nil
+		}
+
+	}
+	return "", errors.New("invalid refresh token")
+}
+
+func (db *DB) RevokeToken(token string) (bool, error) {
+
+	dbStructure, err := db.loadDB()
+
+	if err != nil {
+		log.Print("Error saving access token")
+		return false, errors.New("error revoking token")
+	}
+
+	val, ok := dbStructure.Tokens[token]
+
+	if ok {
+		val.Expires = time.Now().Add(time.Hour * time.Duration(1440))
+		dbStructure.Tokens[token] = val
+
+		err2 := db.writeDB(dbStructure)
+
+		if err2 != nil {
+			log.Print("Error saving access token")
+			return false, errors.New("error revoking token")
+		}
+		return true, nil
+	}
+
+	return false, nil
+}
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
--- main.go
+++ main.go
@@ -4,17 +4,21 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
 	"github.com/nilsboi/Chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits int
+	jwt            string
 }
 
 type returnError struct {
@@ -27,14 +31,19 @@ type returnValid struct {
 	Cleaned_body string `json:"cleaned_body"`
 }
 
+type returnToken struct {
+	// the key will be the name of struct field unless you give it an explicit JSON tag
+	Token string `json:"token"`
+}
+
 type parameters struct {
 	// these tags indicate how the keys in the JSON should be mapped to the struct fields
 	// the struct fields must be exported (start with a capital letter) if you want them parsed
-	Body string `json:"body"`
-	Email string `json:"email"`
-	Password string `json:"password"`
+	Body             string `json:"body"`
+	Email            string `json:"email"`
+	Password         string `json:"password"`
+	ExpiresInSeconds int    `json:"expires_in_seconds,omitempty"`
 }
-
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -50,13 +59,12 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	}
 
 	dat, err := json.Marshal(respError)
-	
+
 	if err != nil {
 		w.WriteHeader(400)
 		return
 	}
 
-	
 	w.WriteHeader(code)
 	w.Write(dat)
 }
@@ -70,25 +78,24 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 
-	
 	w.WriteHeader(code)
 	w.Write(dat)
 }
 
-func checkWords (msg string) string {
+func checkWords(msg string) string {
 
-	msgSlice := strings.Split(msg, " ") 
+	msgSlice := strings.Split(msg, " ")
 
 	for i := 0; i < len(msgSlice); i++ {
-		if strings.ToLower(msgSlice[i]) == "kerfuffle" || 
-		strings.ToLower(msgSlice[i]) == "sharbert" ||
-		strings.ToLower(msgSlice[i])== "fornax" {
+		if strings.ToLower(msgSlice[i]) == "kerfuffle" ||
+			strings.ToLower(msgSlice[i]) == "sharbert" ||
+			strings.ToLower(msgSlice[i]) == "fornax" {
 			msgSlice[i] = "****"
 		}
 	}
 
 	msgCheck := strings.Join(msgSlice, " ")
-	
+
 	return msgCheck
 }
 
@@ -99,14 +106,16 @@ func main() {
 
 	if *dbg {
 		log.Print("Debugging enabled")
-		e := os.Remove("database.json") 
-    if e != nil { 
-				log.Print(e)
-    } 
-
+		e := os.Remove("database.json")
+		if e != nil {
+			log.Print(e)
+		}
 	}
 
-	apiCfg := &apiConfig{}
+	godotenv.Load()
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	apiCfg := &apiConfig{jwt: jwtSecret}
 	mux := http.NewServeMux()
 
 	fileServerHandler := http.FileServer(http.Dir("."))
@@ -144,17 +153,17 @@ func main() {
 		db, err := database.NewDB("database.json")
 
 		if err != nil {
-			respondWithError(w, 400, "Fehler beim Erstellen der DB: " + err.Error())
+			respondWithError(w, 400, "Fehler beim Erstellen der DB: "+err.Error())
 			return
 		}
 
-		chirps, err := db.GetChirps()
-    if err != nil {
-        respondWithError(w, 400, "Fehler beim Abrufen der Chirps: " + err.Error())
-        return
-    }
+		chirps, err := db.GetChirps(0)
+		if err != nil {
+			respondWithError(w, 400, "Fehler beim Abrufen der Chirps: "+err.Error())
+			return
+		}
 
-    respondWithJSON(w, 200, chirps)
+		respondWithJSON(w, 200, chirps)
 
 	})
 
@@ -163,25 +172,23 @@ func main() {
 		db, err := database.NewDB("database.json")
 
 		if err != nil {
-			respondWithError(w, 400, "Fehler beim Erstellen der DB: " + err.Error())
+			respondWithError(w, 400, "Fehler beim Erstellen der DB: "+err.Error())
 			return
 		}
 
 		chirp, err := db.GetChirp(r.PathValue("id"))
 
-    if err != nil {
-        respondWithError(w, 404, "Fehler beim Abrufen der Chirps: " + err.Error())
-        return
-    }
+		if err != nil {
+			respondWithError(w, 404, "Fehler beim Abrufen der Chirps: "+err.Error())
+			return
+		}
 
-    respondWithJSON(w, 200, chirp)
+		respondWithJSON(w, 200, chirp)
 
 	})
 
-	
-
 	mux.HandleFunc("POST /api/chirps", func(w http.ResponseWriter, r *http.Request) {
-	
+
 		decoder := json.NewDecoder(r.Body)
 		params := parameters{}
 
@@ -197,19 +204,21 @@ func main() {
 			return
 		}
 
-	
+		tokenEx := r.Header.Get("Authorization")
+		tokenString := strings.Split(tokenEx, " ")[1]
+
 		db, err := database.NewDB("database.json")
 
 		if err != nil {
-			respondWithError(w, 400, "Fehler beim Erstellen der DB: " + err.Error())
+			respondWithError(w, 400, "Fehler beim Erstellen der DB: "+err.Error())
 			return
 		}
 
 		body := checkWords(params.Body)
-		chirp, err := db.CreateChirp(body)
+		chirp, err := db.CreateChirp(body, tokenString)
 
 		if err != nil {
-			respondWithError(w, 400, "Fehler beim Erstellen des Chrip: " + err.Error())
+			respondWithError(w, 400, "Fehler beim Erstellen des Chrip: "+err.Error())
 			return
 		}
 
@@ -217,9 +226,8 @@ func main() {
 
 	})
 
-
 	mux.HandleFunc("POST /api/users", func(w http.ResponseWriter, r *http.Request) {
-	
+
 		decoder := json.NewDecoder(r.Body)
 		params := parameters{}
 
@@ -233,7 +241,7 @@ func main() {
 		db, err := database.NewDB("database.json")
 
 		if err != nil {
-			respondWithError(w, 400, "Fehler beim Erstellen der DB: " + err.Error())
+			respondWithError(w, 400, "Fehler beim Erstellen der DB: "+err.Error())
 			return
 		}
 
@@ -241,7 +249,7 @@ func main() {
 		user, err := db.CreateUser(params.Email, params.Password)
 
 		if err != nil {
-			respondWithError(w, 400, "Fehler beim Erstellen des User: " + err.Error())
+			respondWithError(w, 400, "Fehler beim Erstellen des User: "+err.Error())
 			return
 		}
 
@@ -250,7 +258,7 @@ func main() {
 	})
 
 	mux.HandleFunc("POST /api/login", func(w http.ResponseWriter, r *http.Request) {
-	
+
 		decoder := json.NewDecoder(r.Body)
 		params := parameters{}
 
@@ -264,18 +272,152 @@ func main() {
 		db, err := database.NewDB("database.json")
 
 		if err != nil {
-			respondWithError(w, 400, "Fehler beim Erstellen der DB: " + err.Error())
+			respondWithError(w, 400, "Fehler beim Erstellen der DB: "+err.Error())
 			return
 		}
 
-		user, err := db.Login(params.Email, params.Password)
+		user, err := db.Login(params.Email, params.Password, apiCfg.jwt)
 
 		if err != nil {
-			respondWithError(w, 401, "Fehler beim Erstellen des User: " + err.Error())
+			respondWithError(w, 401, "Fehler beim Erstellen des User: "+err.Error())
 			return
 		}
 
 		respondWithJSON(w, 200, user)
+
+	})
+
+	mux.HandleFunc("PUT /api/users", func(w http.ResponseWriter, r *http.Request) {
+
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+
+		err := decoder.Decode(&params)
+
+		if err != nil {
+			respondWithError(w, 400, "Something went wrong")
+			return
+		}
+
+		db, err := database.NewDB("database.json")
+
+		if err != nil {
+			respondWithError(w, 400, "Fehler beim Erstellen der DB: "+err.Error())
+			return
+		}
+
+		tokenEx := r.Header.Get("Authorization")
+		tokenString := strings.Split(tokenEx, " ")[1]
+
+		claim, err2 := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(apiCfg.jwt), nil
+		})
+
+		if err2 != nil {
+			respondWithError(w, 401, "Unauthorized: "+err2.Error())
+			return
+		}
+
+		id, err4 := claim.Claims.GetSubject()
+
+		if err4 != nil {
+			respondWithError(w, 401, "Unauthorized: "+err4.Error())
+		}
+
+		idCast, _ := strconv.Atoi(id)
+
+		user, err := db.UpdateUser(params.Email, params.Password, idCast)
+
+		if err != nil {
+			respondWithError(w, 401, "Fehler beim Erstellen des User: "+err.Error())
+			return
+		}
+
+		respondWithJSON(w, 200, user)
+
+	})
+
+	mux.HandleFunc("POST /api/refresh", func(w http.ResponseWriter, r *http.Request) {
+
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+
+		err := decoder.Decode(&params)
+
+		if err != io.EOF {
+			w.WriteHeader(401)
+			return
+		}
+
+		db, err := database.NewDB("database.json")
+
+		if err != nil {
+			respondWithError(w, 400, "Fehler beim Erstellen der DB-Verbindung: "+err.Error())
+			return
+		}
+
+		tokenEx := r.Header.Get("Authorization")
+		tokenString := strings.Split(tokenEx, " ")[1]
+
+		newToken, err := db.RefreshToken(tokenString, apiCfg.jwt)
+
+		if err != nil {
+			respondWithError(w, 401, "Unauthorized: "+err.Error())
+			return
+		}
+
+		respondWithJSON(w, 200, returnToken{Token: newToken})
+	})
+
+	mux.HandleFunc("POST /api/revoke", func(w http.ResponseWriter, r *http.Request) {
+
+		db, err := database.NewDB("database.json")
+
+		if err != nil {
+			respondWithError(w, 400, "Fehler beim Erstellen der DB-Verbindung: "+err.Error())
+			return
+		}
+
+		tokenEx := r.Header.Get("Authorization")
+		tokenString := strings.Split(tokenEx, " ")[1]
+
+		success, err := db.RevokeToken(tokenString)
+
+		if err != nil {
+			respondWithError(w, 400, "Fehler "+err.Error())
+			return
+		}
+
+		if success {
+			w.WriteHeader(204)
+		} else {
+			w.WriteHeader(401)
+		}
+	})
+
+	mux.HandleFunc("DELETE /api/chirps/{ID}", func(w http.ResponseWriter, r *http.Request) {
+		db, err := database.NewDB("database.json")
+
+		if err != nil {
+			respondWithError(w, 400, "Fehler beim Erstellen der DB-Verbindung: "+err.Error())
+			return
+		}
+
+		tokenEx := r.Header.Get("Authorization")
+		tokenString := strings.Split(tokenEx, " ")[1]
+
+		success, err := db.DeleteChirp(tokenString)
+
+		if err != nil {
+			respondWithError(w, 400, "Fehler "+err.Error())
+			return
+		}
+
+		if success {
+			w.WriteHeader(204)
+		} else {
+			w.WriteHeader(403)
+		}
 
 	})
 
